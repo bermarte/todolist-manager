0b3c075d9143623c6d8656e53d7ad5a0fcb72dde
Bermarte <bernardo.martelli@gmail.com>
2021-02-19 17:08:37 +0100

    adding comment to newTodo.js

--- public/src/handlers/newTodo.js
+++ public/src/handlers/newTodo.js
@@ -1,19 +1,21 @@
 'use strict';
 
-
-import { Todo } from '../classes/Todo.js';
+import {
+    Todo
+} from '../classes/Todo.js';
 // Importing validationText function from helpers folder
-import {validationText} from '../helpers/validateTodo.js'
-
+import {
+    validationText
+} from '../helpers/validateTodo.js'
 
 const container = document.querySelector('.todos');
-
 export function submitNewTodo(evt) {
     evt.preventDefault();
     const textField = document.querySelector('#add').value;
-
-    const todo = new Todo(textField, Date.now().toString(), false);
-    container.appendChild(todo.render());
-    //add item to db.json
-    todo.add();
+    if (validationText(textField)) {
+        const todo = new Todo(textField, Date.now().toString());
+        //add item to db.json
+        container.appendChild(todo.render());
+    }
+    document.querySelector('#add').value = "";
 }
